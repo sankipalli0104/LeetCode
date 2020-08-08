683041b2f1c3c5fe309ed9f588776d27b71fb73b
sankipalli0104 <sivalakshmaiah409@gmail.com>
2020-08-08 13:20:46 -0700

    Configured Database Connection

---
+++ src/main/java/practice/leetcode/databaseconnection/ConnectionPool.java
@@ -0,0 +1,75 @@
+package practice.leetcode.databaseconnection;
+
+import org.apache.commons.dbcp2.*;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class ConnectionPool {
+    private static final String URL = "jdbc:mysql://localhost:3306/mysql?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Apple@123";
+
+    private GenericObjectPool<PoolableConnection> connectionPool = null;
+
+    public DataSource setUp() {
+        // Creates a connection factory object which will be use by
+        // the pool to create the connection object. We passes the
+        // JDBC url info, username and password.
+        ConnectionFactory cf = new DriverManagerConnectionFactory(
+            ConnectionPool.URL,
+            ConnectionPool.USERNAME,
+            ConnectionPool.PASSWORD);
+
+        // Creates a PoolableConnectionFactory that will wraps the
+        // connection object created by the ConnectionFactory to add
+        // object pooling functionality.
+        PoolableConnectionFactory pcf = new PoolableConnectionFactory(cf, null);
+        pcf.setValidationQuery("SELECT 1");
+
+        // Creates an instance of GenericObjectPool that holds our
+        // pool of connections object.
+        GenericObjectPoolConfig<PoolableConnection> config = new GenericObjectPoolConfig<>();
+        config.setTestOnBorrow(true);
+        config.setMaxTotal(10);
+        connectionPool = new GenericObjectPool<PoolableConnection>(pcf, config);
+        pcf.setPool(connectionPool);
+
+        return new PoolingDataSource<PoolableConnection>(connectionPool);
+    }
+
+    private GenericObjectPool<PoolableConnection> getConnectionPool() {
+        return connectionPool;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ConnectionPool demo = new ConnectionPool();
+        DataSource dataSource = demo.setUp();
+        demo.printStatus();
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Employee")) {
+            demo.printStatus();
+
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.println("FirstName: " + rs.getString("FirstName"));
+            }
+        }
+
+        demo.printStatus();
+    }
+
+    /**
+     * Prints connection pool status.
+     */
+    private void printStatus() {
+        System.out.println("Max   : " + getConnectionPool().getNumActive() + "; " +
+            "Active: " + getConnectionPool().getNumActive() + "; " +
+            "Idle  : " + getConnectionPool().getNumIdle());
+    }
+}
---
+++ src/main/java/practice/leetcode/databaseconnection/MysqlConnection.java
@@ -0,0 +1,18 @@
+package practice.leetcode.databaseconnection;
+import java.sql.*;
+public class MysqlConnection {
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql?serverTimezone=UTC", "root", "Apple@123");
+			// here sonoo is database name, root is username and password
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from Employee");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
